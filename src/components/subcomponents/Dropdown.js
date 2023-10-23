7d906ada906c748ef8dc4f1f0a8478076dc66b4a
Todd Garrison <toddlgarrison@gmail.com>
2023-10-23 14:09:10 -0400

    Create Dropdown.js

---
+++ src/components/subcomponents/Dropdown.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Dropdown = ({ label, options, selectedOption, onChange}) => {
+    return (
+        <div>
+            <label htmlFor="dropdown"></label>
+            <select 
+                id="dropdown"
+                value={selectedOption}
+                onChange={(event) => onChange(event.target.value)}
+            >
+                <option value="">Select an Option</option>
+                {options.map((option) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+};
+
+export default Dropdown
